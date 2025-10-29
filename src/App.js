8542b6e6b6cbd88da75d509fdf648a280b6de4d3
MBANS8A1 <seanmbng@aol.com>
2025-10-29 11:04:13 +0000

    In handleSplitBill, I am looping over the friends array in setFriends; for each friend object being iterated over, I am checking with the map function to see if the id matches the selectedFriend's id; if there is a match I will add (+/-) the value parameter (determined by whoIsPaying in the FormSplitBill component) to the existing balance.

--- src/App.js
+++ src/App.js
@@ -49,7 +49,13 @@ function App() {
   }
 
   function handleSplitBill(value) {
-    console.log(value);
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
   }
 
   return (
